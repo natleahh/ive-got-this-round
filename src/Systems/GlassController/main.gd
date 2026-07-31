0f6fffd3b9ca64b301eeeb9277d10214a5f9d86a
Aminata Tengbe <71596982+natleahh@users.noreply.github.com>
2026-07-31 17:06:24 +0100

    ref: use preloaded collision shape for gravity zones

--- src/Systems/GlassController/main.gd
+++ src/Systems/GlassController/main.gd
@@ -1,44 +1,31 @@
-extends Node2D
+@tool
+class_name GlassSim extends Node2D
 
 @export var tilt_marker: RigidBody2D
 @export var inner_field: GravityField
 @export var middle_field: GravityField
 @export var outer_field: GravityField
 
-const BASE_GRAVITY: float = 980.0
-
-var _gravity_scale: float = 1
-var _middle_scale: float = -0.1
-var _outer_scale: float = -0.5
-
 @export_category("Gravity Scaling")
 @export var base_gravity_scale: float:
 	set(value):
-		_gravity_scale = value
-		set_gravity()
-	get:
-		return _gravity_scale
+		base_gravity_scale = value
+		update_fields()
 
 @export var middle_gravity_scale: float:
 	set(value):
-		_middle_scale = value
-		set_gravity()
-	get:
-		return _middle_scale
+		middle_gravity_scale = value
+		update_fields()
 
 @export var outer_gravity_scale: float:
 	set(value):
-		_outer_scale = value
-		set_gravity()
-	get:
-		return _outer_scale
+		outer_gravity_scale = value
+		update_fields()
 
-func _ready():
-	set_gravity()
+func update_fields() -> void:
+	inner_field.relative_gravity = base_gravity_scale
+	middle_field.relative_gravity = base_gravity_scale * middle_gravity_scale
+	outer_field.relative_gravity = base_gravity_scale * outer_gravity_scale
 
-func set_gravity() -> void:
-	if not is_node_ready():
-		return
-	inner_field.gravity = BASE_GRAVITY * _gravity_scale
-	middle_field.gravity = BASE_GRAVITY * _gravity_scale * _middle_scale
-	outer_field.gravity = BASE_GRAVITY * _gravity_scale * _outer_scale
+func apply_force(force: Vector2) -> void:
+	tilt_marker.apply_central_force(force)
